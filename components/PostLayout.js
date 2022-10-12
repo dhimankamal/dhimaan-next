5caad6fc9f04c1139c01f69b44f6c549128514fa
kamal <dhimank079@gmail.com>
2022-10-12 08:05:32 +0530

    fix build issue

--- components/PostLayout.js
+++ components/PostLayout.js
@@ -45,7 +45,7 @@ export default function PostLayout ({ pageData }) {
               title='YouTube video player'
               frameBorder='0'
               allow='accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture'
-              allowFullScreen
+            
             ></iframe>
           </div>
         </div>
