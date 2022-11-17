75e538586439611d7624ef9e599963802076e865
kamal <dhimank079@gmail.com>
2022-11-17 07:52:37 +0530

    add analytics

--- pages/_app.js
+++ pages/_app.js
@@ -4,6 +4,7 @@ import 'styles/globals.css'
 import Head from 'next/head'
 import NextNProgress from 'nextjs-progressbar'
 import { ThemeProvider } from 'next-themes'
+import Script from 'next/script'
 
 function MyApp ({ Component, pageProps }) {
   let schema = `{
@@ -19,6 +20,21 @@ function MyApp ({ Component, pageProps }) {
   }`
   return (
     <>
+      <Script
+        strategy='lazyOnload'
+        src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`}
+      />
+
+      <Script id='google-analytics' strategy='lazyOnload'>
+        {`
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+        gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}', {
+        page_path: window.location.pathname,
+        });
+    `}
+      </Script>
       <ThemeProvider attribute='class'>
         <NextNProgress color='#000' />
         <Head>
