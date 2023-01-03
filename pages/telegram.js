dc1605074577bf73c104823a1f9651a952c89a83
kamal <dhimank079@gmail.com>
2023-01-03 20:27:31 +0530

    redirect to telegram

---
+++ pages/telegram.js
@@ -0,0 +1,18 @@
+export const getServerSideProps = async (context) => {
+  
+    return {
+      redirect: {
+        permanent: false,
+        destination: 'https://t.me/+VGG6xXgveTC-ad-2'
+      }
+    }
+  }
+
+export default function Telegram () {
+ 
+  return (
+   <></>
+  )
+}
+
+  
