880c89c0edf820797a46b3222b5f30ec61a30719
kamal <dhimank079@gmail.com>
2023-03-25 20:36:47 +0530

    whatsapp inprpgress

--- pages/ioswhatsapp.jsx
+++ pages/ioswhatsapp.jsx
@@ -1,11 +1,108 @@
 import React from 'react'
-import PostLayout from 'components/PostLayout'
-import { iosWhatsappData } from 'utils/data'
+import { iosWhatsappData as pageData } from 'utils/data'
+import { NextSeo } from 'next-seo'
+import Link from 'next/link'
+import Image from 'next/image'
+import LiteYouTubeEmbed from 'react-lite-youtube-embed'
+import { useRouter } from 'next/router'
 
 export default function Whatsapp() {
+  const router = useRouter()
+  const canonicalUrl = router.pathname
   return (
-    <>
-      <PostLayout pageData={iosWhatsappData} />
-    </>
+    // <div>
+    //   <div></div>
+    // </div>
+    <div className="px-4 py-10">
+      <NextSeo
+        title={pageData.title + '- dhimaan'}
+        titleTemplate={pageData.title}
+        defaultTitle={pageData.title}
+        imageUrl={pageData.imageUrl.src}
+        description={`Hey Whats going on everybody, its Dhiman back with most requested
+      video on our channel ${pageData.title}`}
+        canonical={'https://www.dhimaan.in' + canonicalUrl}
+        openGraph={{
+          url: 'https://www.dhimaan.in/',
+          title: pageData.title,
+          description: `Hey Whats going on everybody, its Dhiman back with most requested
+      video on our channel ${pageData.title}`,
+          images: [
+            {
+              url: 'https://dhimaan-next.vercel.app' + pageData.imageUrl.src,
+              width: 1920,
+              height: 1080,
+              alt: pageData.title,
+            },
+          ],
+        }}
+      />
+
+      <div>
+        <h1 className="text-4xl font-bold mb-4">{pageData?.title}</h1>
+        <span className="">Last Updated: {pageData.dateUpdated}</span>
+      </div>
+
+      <div className="my-10 p-4 border border-black dark:border-white">
+        <Image src={pageData.imageUrl} alt={pageData.title} sizes="100vw" />
+      </div>
+
+      <div>
+        <h2 className="text-3xl font-bold">Latest Version</h2>
+        <ul className="flex justify-between flex-col text-xl items-center py-10 space-y-4">
+          {pageData.dataLinks.map((value, index) => (
+            <li key={index} className="w-full">
+              <Link href={value.link} target="_blank">
+                <div className="border px-2 py-8 border-black dark:border-white hover:bg-black dark:hover:bg-neutral-600 hover:text-white w-full text-sm md:text-2xl transition-color duration-500">
+                  {value.title}
+                </div>
+              </Link>
+            </li>
+          ))}
+        </ul>
+      </div>
+      {pageData?.addons && (
+        <div>
+          <h2 className="text-3xl font-bold">Add ons</h2>
+          <ul className="flex justify-between flex-col text-xl items-center py-10 space-y-4">
+            {pageData.addons.map((value, index) => (
+              <li key={index} className="w-full">
+                <Link
+                  href={value.link}
+                  target="_blank"
+                  className="border px-2 py-8 border-black dark:border-white hover:bg-black dark:hover:bg-neutral-600 hover:text-white w-full text-sm md:text-2xl transition-color duration-500"
+                >
+                  {value.title}
+                </Link>
+              </li>
+            ))}
+          </ul>
+        </div>
+      )}
+      <div className="py-4">
+        <h2 className="text-3xl font-bold">Watch Video</h2>
+        <p>
+          Hey Whats going on everybody, its Dhiman back with most requested
+          video on our channel {pageData.title}
+        </p>
+        <div className="my-6 p-4 border border-black dark:border-white">
+          <LiteYouTubeEmbed id={pageData.videoId} title={pageData.title} />
+        </div>
+      </div>
+      <div className="py-4">
+        <h2 className="text-3xl font-bold">About</h2>
+        <p>
+          Dhiman Kamal is a multi-platform technology Channel. We make videos
+          for everyone, not just for techies. And we post them everywhere.
+          Subscribe for a decent helping of tech features, first look and
+          hands-on videos, unboxings, gadget reviews, tips, tricks,Rom
+          Review,Nova Setups and Android Customisations.
+        </p>
+      </div>
+      <div className="py-4">
+        <h2 className="text-3xl font-bold">Tags</h2>
+        <p>{pageData.tags}</p>
+      </div>
+    </div>
   )
 }
