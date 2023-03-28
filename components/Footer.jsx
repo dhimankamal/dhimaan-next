686cfd24af30ef43f161eb5234f36440c5b793f9
kamal <dhimank079@gmail.com>
2023-03-28 19:54:33 +0530

    fix footer

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 
 export default function Footer() {
   return (
-    <footer className="container mx-4 md:mx-auto border-t border-bleack py-8 text-center flex flex-col md:flex-row justify-between items-center">
+    <footer className="md:mx-auto border-t border-bleack py-8 text-center flex flex-col md:flex-row justify-between items-center">
       <Link href="/privacypolicy">Privacy Policy</Link>
       <span className="text-center">Design by kamal</span>
     </footer>
