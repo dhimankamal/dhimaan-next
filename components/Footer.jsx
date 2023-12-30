820fa7d53879003773dda8308fd68f15bffb328c
kamal <dhimank079@gmail.com>
2023-12-30 17:02:33 +0530

    add copyright

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -5,7 +5,7 @@ export default function Footer() {
   return (
     <footer className="container md:mx-auto border-t border-bleack py-8 text-center flex flex-col md:flex-row justify-between items-center">
       <Link href="/privacypolicy">Privacy Policy</Link>
-      <span className="text-center">Design by kamal</span>
+      <span className="text-center">Design by <Link href={"https://www.itskamal.dev/"}>kamal </Link> </span>
     </footer>
   )
 }
